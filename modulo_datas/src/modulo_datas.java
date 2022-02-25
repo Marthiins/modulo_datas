dd6281f8b820b427e3b6f1e7c0970fd876467b73
Marthiins <sergio.marthiins@gmail.com>
2022-02-25 15:50:34 -0300

    Formatando datas com o SimpleDateFormat

--- modulo_datas/src/modulo_datas.java
+++ modulo_datas/src/modulo_datas.java
@@ -1,19 +1,23 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class modulo_datas {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws ParseException {
+
 		Date date = new Date();
-		
-		System.out.println("Data em milisegundos: " + date.getTime());
-		
-		System.out.println("Dia do mês: " + date.getDate());
-		
-		System.out.println("Horas : " + date.getHours());
-		
-		System.out.println("Horas : " + (date.getYear() + 1900));/*Ano que que ser somado ao 1900*/
-		
+
+		/*------------------Simple Date Format*/
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm.ss");// Desde jeito para gravar no banco de dados
+		System.out.println("Data formato para banco de dados: " + simpleDateFormat.format(date));																	// dados
+
+		SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd/MM/yyyy HH:mm.ss");// Para teste
+		System.out.println("Data atual padrão é string: " + simpleDateFormat2.format(date));
+
+		 simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		 System.out.println("Objeto data: " + simpleDateFormat.parse("25/02/2022"));
 	}
 
 }
