905bb7b572d3c10dc3a4cf22267ec52ef6953d6c
Marthiins <sergio.marthiins@gmail.com>
2022-02-25 15:22:29 -0300

    Conhecendo o Objeto Date

--- modulo_datas/src/modulo_datas.java
+++ modulo_datas/src/modulo_datas.java
@@ -1,4 +1,19 @@
+import java.util.Date;
 
 public class modulo_datas {
+	
+	public static void main(String[] args) {
+		
+		Date date = new Date();
+		
+		System.out.println("Data em milisegundos: " + date.getTime());
+		
+		System.out.println("Dia do mês: " + date.getDate());
+		
+		System.out.println("Horas : " + date.getHours());
+		
+		System.out.println("Horas : " + (date.getYear() + 1900));/*Ano que que ser somado ao 1900*/
+		
+	}
 
 }
