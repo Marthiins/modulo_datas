f98a12b668f62e3f9fedede33d57f9a658cc93e8
Marthiins <sergio.marthiins@gmail.com>
2022-02-25 22:53:11 -0300

    Gerando parcelas mensais e suas datas

---
+++ modulo_datas/src/DatasEmJava1.java
@@ -0,0 +1,25 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatasEmJava1 {
+
+	public static void main(String[] args) throws ParseException {
+
+		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("25/02/2022");/*Data inicial*/
+
+		Calendar calendar = Calendar.getInstance();/*Instaciar um objeto calendario*/
+		calendar.setTime(dataInicial);/*Passo a data inicial*/
+
+		for (int parcela = 1; parcela <= 12; parcela++) {
+			calendar.add(Calendar.MONTH, 1); /* A partir vou adicionar 1 mês */
+		
+		System.out.println("Parcela de numero: " + parcela + " Vencimento é em : "
+		+ new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
+
+	}
+
+}
